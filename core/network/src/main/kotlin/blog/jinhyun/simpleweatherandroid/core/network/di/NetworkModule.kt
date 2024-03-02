4897741c66b8bc6052dfff4d46b63cc098ebe35d
주진현 <joojinhyun00@gmail.com>
2024-03-03 02:19:14 +0900

    network 라이브러리 di 추가

---
+++ core/network/src/main/kotlin/blog/jinhyun/simpleweatherandroid/core/network/di/NetworkModule.kt
@@ -0,0 +1,38 @@
+package blog.jinhyun.simpleweatherandroid.core.network.di
+
+import androidx.core.os.trace
+import blog.jinhyun.simpleweatherandroid.core.network.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient = trace("OkHttpClient") {
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .apply {
+                        if (BuildConfig.DEBUG) {
+                            setLevel(HttpLoggingInterceptor.Level.BODY)
+                        }
+                    },
+            )
+            .build()
+    }
+}

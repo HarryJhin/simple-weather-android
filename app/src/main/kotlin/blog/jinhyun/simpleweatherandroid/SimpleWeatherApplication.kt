b9336b03305b4cc25f6bc11bb09bc835e72b0140
주진현 <joojinhyun00@gmail.com>
2024-03-03 01:43:10 +0900

    Hilt DI를 위한 HiltAndroidApp 적용

--- app/src/main/kotlin/blog/jinhyun/simpleweatherandroid/SimpleWeatherApplication.kt
+++ app/src/main/kotlin/blog/jinhyun/simpleweatherandroid/SimpleWeatherApplication.kt
@@ -1,5 +1,7 @@
 package blog.jinhyun.simpleweatherandroid
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SimpleWeatherApplication : Application()
